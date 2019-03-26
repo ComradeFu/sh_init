a7cc13244cdcca87a5fc53200519ac52c00dba01
fuguangze <natsusao@gmail.com>
2019-03-26 17:58:23 +0800

    chg:让 bootstrap 进行下载并自动部署

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,23 @@
 # 父级目录
 # 固定的喔
-basepath="$HOME/.local"
+basepath="$HOME/.local/config"
+basetmppath="$basepath/tmp"
+
 tempbashrc="$HOME/.bashrc"
 
-if [ ! -d $basepath ]; then
-    mkdir -p basepath
+# 创建好目录
+if [ ! -d $basetmppath ]; then
+    mkdir -p basetmppath
 fi
+
+cd $basetmppath
+
+# 从gitlab上下载下来完整的包，并切解压缩
+curl -O "https://codeload.github.com/ComradeFu/sh_init/zip/master"
+unzip master
+
+# 讲固定名字下的文件夹拷贝到basepath
+cp -rf ./sh_init-master $basepath
 
 if [ ! -f $tempbashrc ]; then
     touch $tempbashrc
@@ -17,3 +29,6 @@ echo "source ${basepath}/init.sh" >> $tempbashrc
 #     touch $tempprofilerc
 # fi
 # echo "source ${basepath}/init.sh" > $tempprofilerc
+
+# 删除临时文件夹
+rm -rf $basetmppath
