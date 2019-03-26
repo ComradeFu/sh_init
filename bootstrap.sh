55f8b583601924ae68b431f28fd53e0f5d5fef94
fuguangze <natsusao@gmail.com>
2019-03-26 18:05:54 +0800

    chg:添加日志进行打印 

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,22 @@
 # 父级目录
 # 固定的喔
+echo "自动配置脚本开始执行..."
+
 basepath="$HOME/.local/config"
 basetmppath="$basepath/tmp"
 
 tempbashrc="$HOME/.bashrc"
 
+git="https://codeload.github.com/ComradeFu/sh_init/zip/master"
+git_name="sh_init-master"
+
 # 创建好目录
+if [ ! -d $basepath ]; then
+    mkdir -p basepath
+    echo "创建本地配置目录${basepath}"
+fi
+
+# 临时目录
 if [ ! -d $basetmppath ]; then
     mkdir -p basetmppath
 fi
@@ -13,16 +24,22 @@ fi
 cd $basetmppath
 
 # 从gitlab上下载下来完整的包，并切解压缩
-curl -O "https://codeload.github.com/ComradeFu/sh_init/zip/master"
+curl -O $git
 unzip master
 
+echo "完成git版本库下载:${git}"
+
 # 讲固定名字下的文件夹拷贝到basepath
-cp -rf ./sh_init-master $basepath
+cp -rf ./${git_name} $basepath
+
+echo "复制至:${basepath}"
 
 if [ ! -f $tempbashrc ]; then
     touch $tempbashrc
 fi
 echo "source ${basepath}/init.sh" >> $tempbashrc
+
+echo "导入 bashrc 完成"
 
 # tempprofilerc="~/.profile"
 # if [ ! -f $tempprofilerc ]; then
@@ -32,3 +49,5 @@ echo "source ${basepath}/init.sh" >> $tempbashrc
 
 # 删除临时文件夹
 rm -rf $basetmppath
+
+echo "自动配置完成"
